6568b5e7ccea90992e67d2afafaef28d223c9c81
danielhurtz <daniel.anjos@protonmail.com>
2022-06-18 22:15:35 -0300

    Primeiro Commit

---
+++ src/tiposDeFuncao.js
@@ -0,0 +1,23 @@
+// tipos de funcao
+
+// declarativas 
+/* function funcao() {
+    console.log('Sou uma mensagem de uma funcao declarativa)
+}
+
+funcao(); */
+
+// expressao de funcao
+// com nomeacao
+
+/* var funcao = function() {
+    console.log('Sou uma mensagem de funcao de expessao')
+}
+
+funcao();
+
+// arrow function
+var funcao = () => {
+    console.log('Sou um arrow function');
+}
+*/
